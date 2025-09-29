432227cf20310c90f36edf4e849b7a0e6b1d7519
Tanish Raorane <tanishraorane4@gmail.com>
2025-09-29 19:23:19 +0100

    .

--- VirtualProductionNew/Assets/io.ossrs/Samples/Player/SrsPlayer.cs
+++ VirtualProductionNew/Assets/io.ossrs/Samples/Player/SrsPlayer.cs
@@ -84,11 +84,13 @@ public class SrsPlayer : MonoBehaviour
                     receiveImage.texture = tex; // Directly assign
                     receiveImage.enabled = true;
 
-                    // Set image size based on video
+                    //Set image size based on video
                     var width = tex.width < 1280 ? tex.width : 1280;
                     var height = tex.width > 0 ? width * tex.height / tex.width : 720;
-                    receiveImage.rectTransform.sizeDelta = new Vector2(width, height);
+                    //receiveImage.rectTransform.sizeDelta = new Vector2(width, height);
+                    receiveImage.rectTransform.sizeDelta = new Vector2(3, 3);
                     receiveImage.rectTransform.localScale = Vector3.one;
+
                 };
             }
 
